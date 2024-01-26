bc84265ab10cdc0275dcd4764d75945acf212c82
KongChengdesu <77406643+KongChengdesu@users.noreply.github.com>
2024-01-26 15:31:56 -0800

    Update restart-service.sh

--- restart-service.sh
+++ restart-service.sh
@@ -5,7 +5,7 @@ pm2 delete TelegramStocker
 pm2 save
 rm -rf ~/apps/TelegramStocker
 mkdir ~/apps/TelegramStocker
-cp -r ~/actions-runner/_work/TelegramStocker/TelegramStocker/* ~/apps/TelegramStocker
+cp -r ~/stocker-runner/_work/TelegramStocker/TelegramStocker/* ~/apps/TelegramStocker
 cp ~/stocker.env ~/apps/TelegramStocker/.env
 cd ~/apps/TelegramStocker
 pm2 start ecosystem.config.js
